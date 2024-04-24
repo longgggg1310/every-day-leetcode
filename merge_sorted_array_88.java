26bc2d998d779c60c3928ecebb42a112dcdf9deb
Long Vu Dang <74718176+longgggg1310@users.noreply.github.com>
2024-04-24 11:58:48 +0700

    update 88_merge_element

--- merge_sorted_array_88.java
+++ merge_sorted_array_88.java
@@ -1,33 +1,31 @@
 public class merge_sorted_array_88 {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < nums2.length; i++) {
-            insert_into_array(i, nums1, m);
-            m++;
-        }
-    }
-
-    private static void insert_into_array(int x, int[] a, int m) {
-        boolean found_value = false;
-        for (int k = 0; k < m; k++) {
-            if (a[k] > x) {
-                found_value = true;
-                for (int i = m - 1; i >= k; i--) {
-                    a[i + 1] = a[i];
-                }
-                a[k] = x;
-                break;
+        int i = m - 1; // Index of last element in nums1
+        int j = n - 1; // Index of last element in nums2
+        int k = (m + n) - 1; // Index of last position in nums1
+        while (k >= 0) {
+            if (j < 0) {
+                nums1[k] = nums1[i];
+                i--;
+            } else if (i < 0) {
+                nums1[k] = nums2[j];
+                j--;
+            } else if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
-        }
-        if (found_value == false) {
-            a[m] = x;
-
+            k--;
         }
     }
 
     public static void main(String[] args) {
-        int[] n1 = { 2, 3, 4, 5, 0, 0, 0 };
-        int[] n2 = { 0, 3, 6 };
-        merge(n1, 4, n2, 3);
+        int[] n1 = { 1, 2, 3, 0, 0, 0 };
+        int[] n2 = { 2, 3, 6 };
+        merge(n1, 3, n2, 3);
+        System.out.print("1`23");
 
     }
 }
